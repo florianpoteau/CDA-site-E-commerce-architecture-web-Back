2219bd73c5ff041037068152ddae2ca1c1a84789
Poteau Florian <florianpoteau59@gmail.com>
2024-03-12 14:01:04 +0100

    feat(Initilisation): initialisation du projet avec Spring et Maven

---
+++ site-e-commerce/src/main/java/co/simplon/siteecommerce/SiteECommerceApplication.java
@@ -0,0 +1,13 @@
+package co.simplon.siteecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteECommerceApplication.class, args);
+	}
+
+}
