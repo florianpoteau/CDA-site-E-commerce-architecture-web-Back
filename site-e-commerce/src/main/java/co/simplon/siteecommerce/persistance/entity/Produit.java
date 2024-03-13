8cf67f024003a78fa7f367fded7dd5156d58b85d
Poteau Florian <florianpoteau59@gmail.com>
2024-03-13 09:56:30 +0100

    add: configuration de la base de données

---
+++ site-e-commerce/src/main/java/co/simplon/siteecommerce/persistance/entity/Produit.java
@@ -0,0 +1,42 @@
+package co.simplon.siteecommerce.persistance.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Produit")
+public class Produit {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String description;
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
