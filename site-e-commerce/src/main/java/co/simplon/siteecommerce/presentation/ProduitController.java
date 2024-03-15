fa5d260882e1ab5e8b9ab022715e304c4fa2ef4f
Poteau Florian <florianpoteau59@gmail.com>
2024-03-15 10:58:20 +0100

    fix: Docker-compose.yml connexion de la bdd

--- site-e-commerce/src/main/java/co/simplon/siteecommerce/presentation/ProduitController.java
+++ site-e-commerce/src/main/java/co/simplon/siteecommerce/presentation/ProduitController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,7 @@ public class ProduitController {
         this.produitService = produitService;
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/produit")
     public List<ProduitDTO> listProduits() {
         return produitService.lire();
