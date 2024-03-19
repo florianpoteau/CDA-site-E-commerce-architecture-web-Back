0d38fb70b72131c4a7650b4b65c884155dbedb5a
Poteau Florian <florianpoteau59@gmail.com>
2024-03-19 15:53:01 +0100

    add: schéma + read-me

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # CDA-site-E-commerce-architecture-web-Back
+
+## Objectif
+
+L'objectif du projet est de créer une api Rest avec Spring en utilisant le n-tiers, ainsi que de créer un schéma montrant l'architecture.
+Vous trouverez dans ce repo:
+
+- Le projet
+- Docker-compose
+- Un schéma de l'architecture
+
+## Initialisation
+
+Tout d'abord, vous devez clone le repository avec cette commande :
+
+```bash
+git clone git@github.com:florianpoteau/CDA-site-E-commerce-architecture-web-Back.git
+```
+
+Ensuite vous devez faire :
+
+```bash
+docker compose up
+```
+
+```bash
+cd site-e-commerce
+```
+
+Puis lancer le serveur Spring.
+Pour avoir l'interface utilisateur: vous devrez cloner ce repository: [CDA-site-E-commerce-architecture-web-Front](https://github.com/florianpoteau/CDA-site-E-commerce-architecture-web-Front)
+
+## Collaborateurs
+
+-**_Florian Poteau_**
+
+<a href="https://github.com/florianpoteau"><img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white"></img></a>
+
+-**_Saif Ali_**
+
+<a href="https://github.com/Saif-ALI96"><img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white"></img></a>
--- script.sql
+++ script.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database cluster dump
 --
 
--- Started on 2024-03-16 14:42:26
+-- Started on 2024-03-19 14:54:31
 
 SET default_transaction_read_only = off;
 
@@ -12,7 +12,6 @@ SET standard_conforming_strings = on;
 --
 -- Roles
 --
-
 
 
 --
@@ -41,7 +40,7 @@ SET standard_conforming_strings = on;
 -- Dumped from database version 14.1
 -- Dumped by pg_dump version 16.1
 
--- Started on 2024-03-16 14:42:26
+-- Started on 2024-03-19 14:54:32
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -74,7 +73,7 @@ REVOKE USAGE ON SCHEMA public FROM PUBLIC;
 GRANT ALL ON SCHEMA public TO PUBLIC;
 
 
--- Completed on 2024-03-16 14:42:27
+-- Completed on 2024-03-19 14:54:32
 
 --
 -- PostgreSQL database dump complete
@@ -91,7 +90,7 @@ GRANT ALL ON SCHEMA public TO PUBLIC;
 -- Dumped from database version 14.1
 -- Dumped by pg_dump version 16.1
 
--- Started on 2024-03-16 14:42:27
+-- Started on 2024-03-19 14:54:32
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -108,8 +107,6 @@ SET row_security = off;
 -- TOC entry 3319 (class 1262 OID 16384)
 -- Name: ecommerce; Type: DATABASE; Schema: -; Owner: root
 --
-
-
 
 
 ALTER DATABASE ecommerce OWNER TO root;
@@ -138,7 +135,7 @@ SET row_security = off;
 ALTER SCHEMA public OWNER TO root;
 
 --
--- TOC entry 212 (class 1259 OID 16395)
+-- TOC entry 209 (class 1259 OID 16385)
 -- Name: my_seq; Type: SEQUENCE; Schema: public; Owner: root
 --
 
@@ -157,7 +154,7 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- TOC entry 209 (class 1259 OID 16385)
+-- TOC entry 210 (class 1259 OID 16386)
 -- Name: produit; Type: TABLE; Schema: public; Owner: root
 --
 
@@ -173,7 +170,7 @@ CREATE TABLE public.produit (
 ALTER TABLE public.produit OWNER TO root;
 
 --
--- TOC entry 210 (class 1259 OID 16390)
+-- TOC entry 211 (class 1259 OID 16391)
 -- Name: produit_id_seq; Type: SEQUENCE; Schema: public; Owner: root
 --
 
@@ -190,7 +187,7 @@ ALTER SEQUENCE public.produit_id_seq OWNER TO root;
 
 --
 -- TOC entry 3321 (class 0 OID 0)
--- Dependencies: 210
+-- Dependencies: 211
 -- Name: produit_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: root
 --
 
@@ -198,7 +195,7 @@ ALTER SEQUENCE public.produit_id_seq OWNED BY public.produit.id;
 
 
 --
--- TOC entry 211 (class 1259 OID 16394)
+-- TOC entry 212 (class 1259 OID 16392)
 -- Name: produit_seq; Type: SEQUENCE; Schema: public; Owner: root
 --
 
@@ -213,7 +210,7 @@ CREATE SEQUENCE public.produit_seq
 ALTER SEQUENCE public.produit_seq OWNER TO root;
 
 --
--- TOC entry 3168 (class 2604 OID 16391)
+-- TOC entry 3168 (class 2604 OID 16393)
 -- Name: produit id; Type: DEFAULT; Schema: public; Owner: root
 --
 
@@ -221,24 +218,22 @@ ALTER TABLE ONLY public.produit ALTER COLUMN id SET DEFAULT nextval('public.prod
 
 
 --
--- TOC entry 3310 (class 0 OID 16385)
--- Dependencies: 209
+-- TOC entry 3311 (class 0 OID 16386)
+-- Dependencies: 210
 -- Data for Name: produit; Type: TABLE DATA; Schema: public; Owner: root
 --
 
 COPY public.produit (id, description, image, prix, titre) FROM stdin;
-1	 la PS5 propose des graphismes époustouflants et une vitesse de chargement ultra-rapide grâce à son SSD intégré. Elle est équipée d'un processeur AMD Ryzen Zen 2 avec 8 cœurs, capable de fournir des performances de jeu fluides et réactives	https://images.unsplash.com/photo-1606144042614-b2417e99c4e3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D	429.99	Playstation 5
-2	Les caractéristiques d'un costume de qualité incluent des tissus haut de gamme comme la laine, le coton ou le lin, offrant confort et durabilité. Les coupes et les finitions soignées assurent une silhouette élégante et une apparence soignée	https://images.unsplash.com/photo-1479064555552-3ef4979f8908?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D	149.99	Costume
-3	Fabriquées avec des matériaux de haute qualité et une technologie innovante, les chaussures Nike offrent un ajustement précis, un amorti réactif et une adhérence supérieure pour une expérience de course ou d'entraînement optimale.	https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D	99.99	Basket Nike
-4	Fabriquées avec des matériaux de haute qualité et une technologie innovante, les chaussures Nike offrent un ajustement précis, un amorti réactif et une adhérence supérieure pour une expérience de course ou d'entraînement optimale.	https://images.unsplash.com/photo-1549298916-b41d501d3772?q=80&w=2012&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D	149.99	Basket Nike Air
-5	La Nintendo Switch est une console de jeu vidéo polyvalente et innovante qui offre une expérience de jeu unique. Grâce à son design novateur	https://images.unsplash.com/photo-1614925379183-3ce5e634a612?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D	145.99	Nintendo Switch
 6	Découvrez le compagnon idéal pour conquérir les pistes enneigées - nos skis de haute performance vous offrent une expérience de glisse inégalée. Fabriqués avec les meilleurs matériaux et une technologie de pointe,	https://images.unsplash.com/photo-1557977398-18b39bf47159?q=80&w=1890&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D	145.99	Skis
+2	Découvrez notre sélection d'équipements de ski dernier cri pour des descentes sans pareil. De la pointe de la technologie à la performance inégalée, notre gamme vous garantit confort, sécurité et style sur les pistes. Transformez chaque virage en une expérience inoubliable avec nos produits de haute qualité, conçus pour les passionnés de glisse. Prêt à dévaler les pentes avec confiance ? Rejoignez-nous et laissez votre aventure commencer dès aujourd'hui.	https://cdn.pixabay.com/photo/2015/09/09/17/57/ski-932188_1280.jpg	439.99	Equipement de skis
+651	Imaginez un skieur intrépide, chevauchant les pentes en quête d'adrénaline. Soudain, une cascade de neige, un bruissement, et voilà qu'il dégringole dans un tourbillon de poudreuse. Mais pas de panique ! Avec notre paire d'équipements de ski haut de gamme, chutes comme celle-ci se transforment en moments mémorables. Conçue pour résister aux défis des pistes les plus exigeantes, notre paire vous assure stabilité et sécurité à chaque descente. Prêt à vous relever avec style et détermination ? Optez pour notre paire et domptez les montagnes avec confiance.	https://cdn.pixabay.com/photo/2017/12/29/20/25/snow-3048590_960_720.jpg	199.99	Tombeur de ski
+701	\nPlongez dans l'hiver avec nos gants de ski conçus pour défier les éléments les plus rigoureux. Fabriqués avec des matériaux haut de gamme, ils offrent une combinaison parfaite de chaleur, de confort et de protection contre le froid glacial. Leurs caractéristiques techniques avancées garantissent une adhérence optimale sur les bâtons et une imperméabilité à toute épreuve, vous permettant de rester concentré sur vos performances. Affrontez les descentes les plus difficiles avec confiance grâce à nos gants de ski, votre partenaire indispensable sur les pistes enneigées.	https://cdn.pixabay.com/photo/2022/01/12/16/35/ski-glove-6933354_1280.jpg	69.99	Gants de ski
 \.
 
 
 --
 -- TOC entry 3322 (class 0 OID 0)
--- Dependencies: 212
+-- Dependencies: 209
 -- Name: my_seq; Type: SEQUENCE SET; Schema: public; Owner: root
 --
 
@@ -247,7 +242,7 @@ SELECT pg_catalog.setval('public.my_seq', 1, true);
 
 --
 -- TOC entry 3323 (class 0 OID 0)
--- Dependencies: 210
+-- Dependencies: 211
 -- Name: produit_id_seq; Type: SEQUENCE SET; Schema: public; Owner: root
 --
 
@@ -256,15 +251,15 @@ SELECT pg_catalog.setval('public.produit_id_seq', 8, true);
 
 --
 -- TOC entry 3324 (class 0 OID 0)
--- Dependencies: 211
+-- Dependencies: 212
 -- Name: produit_seq; Type: SEQUENCE SET; Schema: public; Owner: root
 --
 
-SELECT pg_catalog.setval('public.produit_seq', 601, true);
+SELECT pg_catalog.setval('public.produit_seq', 701, true);
 
 
 --
--- TOC entry 3170 (class 2606 OID 16393)
+-- TOC entry 3170 (class 2606 OID 16395)
 -- Name: produit produit_pkey; Type: CONSTRAINT; Schema: public; Owner: root
 --
 
@@ -282,7 +277,7 @@ REVOKE USAGE ON SCHEMA public FROM PUBLIC;
 GRANT ALL ON SCHEMA public TO PUBLIC;
 
 
--- Completed on 2024-03-16 14:42:27
+-- Completed on 2024-03-19 14:54:32
 
 --
 -- PostgreSQL database dump complete
@@ -301,7 +296,7 @@ GRANT ALL ON SCHEMA public TO PUBLIC;
 -- Dumped from database version 14.1
 -- Dumped by pg_dump version 16.1
 
--- Started on 2024-03-16 14:42:28
+-- Started on 2024-03-19 14:54:32
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -334,13 +329,13 @@ REVOKE USAGE ON SCHEMA public FROM PUBLIC;
 GRANT ALL ON SCHEMA public TO PUBLIC;
 
 
--- Completed on 2024-03-16 14:42:28
+-- Completed on 2024-03-19 14:54:32
 
 --
 -- PostgreSQL database dump complete
 --
 
--- Completed on 2024-03-16 14:42:28
+-- Completed on 2024-03-19 14:54:33
 
 --
 -- PostgreSQL database cluster dump complete
